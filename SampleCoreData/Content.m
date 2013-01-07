ad2dd1380e9bb9a81d5ede75bff1a1ae29d4e19d
FuruyamaTakeshi <rx13b654x2@gmail.com>
2013-01-08 08:15:55 +0900

    Revert "optimize"
    This reverts commit a042ae8958464c92addb4a5f2fac9e5a247bbc73.

---
+++ SampleCoreData/Content.m
@@ -0,0 +1,23 @@
+//
+//  Content.m
+//  SampleCoreData
+//
+//  Created by 古山 健司 on 12/12/24.
+//  Copyright (c) 2012年 TF. All rights reserved.
+//
+
+#import "Content.h"
+#import "RCEntry.h"
+
+
+@implementation Content
+
+@dynamic link;
+@dynamic title;
+@dynamic status;
+@dynamic isDone;
+@dynamic deadLine;
+@dynamic priority;
+@dynamic entry;
+
+@end
